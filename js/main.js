5b78353e3c29c0297b0f5b870b5aaa5b3c8dcd1b
alelignac <alignac@gmail.com>
2023-03-08 00:09:06 -0300

    Segunda Entrega JavaScript v2.0

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ let concat="";
 let flag = 0;
 let mensaje;
 
-
+//Objeto Usuario
 function Usuario(tuNombre, tuEdad, tuDireccion){
    this.nombre = tuNombre;
    this.edad = tuEdad;
    this.direccion = tuDireccion;
 }
-
+//Array Bebidas
 const bebidas = [
   { id: 1, nombre: "GANCIA", precio: 1200, cant: 100 },
   { id: 2, nombre: "FERNET", precio: 1900, cant: 200 },
@@ -25,146 +25,147 @@ const bebidas = [
   { id: 6, nombre: "GASEOSA", precio: 200, cant: 120 },
 ];
 
-
 while(contador < 4){
-  console.log(contador);
-  for(contador = 1 ;contador < 4 ;  contador++){
-    if(flag === 0){
-        nombre = prompt("Ingrese el nombre");
-        edad = parseInt(prompt("Ingrese la edad"));
-        direccion = prompt("Ingrese la direccion");
-        const persona = new Usuario(nombre, edad, direccion);
-        flag = 1;
-    }
-    if(edad < 18){
-      alert("No puede ingresar al sitio, ingrese su edad nuevamente");
-      flag = 0;
-    }else if(edad >= 18){
-      pedido=parseInt(prompt(nombre+", Por favor, ingrese 1 para realizar un pedido o 2 consultar disponibilidad de bebidas"))
-      if(pedido === 1){
-          salida = 1;
-          let ingreso = prompt("Ingrese la bebida que desea comprar: "+bebidas.map((bebida) => bebida.nombre)).toLocaleUpperCase();
-          console.log(ingreso);
-          switch(ingreso){
-            case "GANCIA":
-              cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
-              if (cantidad>0){
-                valor=1200;
-                importe=valor*cantidad;
-                alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
-              }else{
-                alert("El valor ingresado es incorrecto");
-              }
-              break;
-
-            case "FERNET":
-              cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
-              if (cantidad>0){
-                valor=1900;
-                importe=valor*cantidad;
-                alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
-              }else{
-                alert("El valor ingresado es incorrecto");
-              }
-              break;
-            
-            case "GIN":
-              cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
-              if (cantidad>0){
-                valor=5000;
-                importe=valor*cantidad;
-                alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
-              }else{
-                alert("El valor ingresado es incorrecto");
-              }
-            break;
-            
-            case "VODKA":
-              cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
-              if (cantidad>0){
-                valor=1000;
-                importe=valor*cantidad;
-                alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe)+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!";
-              }else{
-                alert("El valor ingresado es incorrecto");
-              }
-              break;
-            
-            case "VINO":
-              cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
-              if (cantidad>0){
-                valor=350;
-                importe=valor*cantidad;
-                alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
-              }else{
-                alert("El valor ingresado es incorrecto");
-              }
-              break;
-
-            case "GASEOSA":
-              cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
-              if (cantidad>0){
-                valor=200;
-                importe=valor*cantidad;
-                alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
-              }else{
-                alert("El valor ingresado es incorrecto");
-              }
-              break;
-
-            default:
-              alert("Bebida no encontrada!");
-              salida = 0;
-          }
-      }else if(pedido === 2){
-        let consulta = prompt("Ingrese la bebida que desea consultar: "+bebidas.map((bebida) => bebida.nombre)).toLocaleUpperCase();
-        let bebida = bebidas.find(item => item.nombre === consulta);
-        if(bebida != undefined){
-        mensaje = `
-          Nombre: ${bebida.nombre}
-          Precio: $${bebida.precio}
-          Cantidad: ${bebida.cant}
-        `;
-        }else{
-          alert("la opcion ingresada es invalida");
-          break
-        }
-        alert(mensaje);
-        recorroArray = parseInt(prompt("Quiere ver todos nuestros productos, cantidades y precios? ingrese 1 por Si, 2 por No"));
-        if(recorroArray === 1){
-          bebidas.forEach(bebida => {
-            concat = concat + ("\n Nombre: "+bebida.nombre + ", Cantidad: "+bebida.cant+", Precio: $"+bebida.precio);
-          });
-          alert(concat)
-          compra = parseInt((prompt("\n Si desea alguno de nuestros productos, ingrese 1, para salir ingrese 2 o precione enter")));
-          contador = contador + 1 
-          if(compra === 1){
-            flag = 1;
-            break
-          }else{
-            alert("Muchas gracias por contactarnos!")
-            break
-          }
-        }else if(recorroArray === 2){
-          alert("Muchas gracias por contactarnos!")
-          contador = 5
-          break
-        }else{
-          alert("La opcion ingresada no es valida");
-        }
-      }else{
-        alert("La opcion ingresada no es valida");
+    console.log(contador);
+    for(contador = 1 ;contador < 4 ;  contador++){
+      if(flag === 0){
+          nombre = prompt("Ingrese el nombre");
+          edad = parseInt(prompt("Ingrese la edad"));
+          direccion = prompt("Ingrese su direccion de correo electronico");
+          const persona = new Usuario(nombre, edad, direccion);
+          flag = 1;
       }
-    }else{
-      alert("El dato ingresado no es correcto");
-      flag = 0;
-    }
-  if(salida == 1){
-    contador = 5;
-    break;
-  }
-  } 
+      if(edad < 18){
+          alert("No puede ingresar al sitio, ingrese su edad nuevamente");
+          flag = 0;
+      }else if(edad >= 18){
+          pedido=parseInt(prompt(nombre+", Por favor, ingrese 1 para realizar un pedido o 2 consultar disponibilidad de bebidas"))
+          if(pedido === 1){
+              salida = 1;
+              let ingreso = prompt("Ingrese la bebida que desea comprar: "+bebidas.map((bebida) => bebida.nombre)).toLocaleUpperCase();
+              console.log(ingreso);
+              //inicio Switch para eleccion de bebidas
+              switch(ingreso){
+                case "GANCIA":
+                  cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
+                  if (cantidad>0){
+                    valor=1200;
+                    importe=valor*cantidad;
+                    alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
+                  }else{
+                    alert("El valor ingresado es incorrecto");
+                  }
+                  break;
+
+                case "FERNET":
+                  cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
+                  if (cantidad>0){
+                    valor=1900;
+                    importe=valor*cantidad;
+                    alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
+                  }else{
+                    alert("El valor ingresado es incorrecto");
+                  }
+                  break;
+                
+                case "GIN":
+                  cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
+                  if (cantidad>0){
+                    valor=5000;
+                    importe=valor*cantidad;
+                    alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
+                  }else{
+                    alert("El valor ingresado es incorrecto");
+                  }
+                  break;
+                
+                case "VODKA":
+                  cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
+                  if (cantidad>0){
+                    valor=1000;
+                    importe=valor*cantidad;
+                    alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe)+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!";
+                  }else{
+                    alert("El valor ingresado es incorrecto");
+                  }
+                  break;
+                
+                case "VINO":
+                  cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
+                  if (cantidad>0){
+                    valor=350;
+                    importe=valor*cantidad;
+                    alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
+                  }else{
+                    alert("El valor ingresado es incorrecto");
+                  }
+                  break;
+
+                case "GASEOSA":
+                  cantidad=prompt("ingrese la cantidad de "+ingreso +" que desea comoprar");
+                  if (cantidad>0){
+                    valor=200;
+                    importe=valor*cantidad;
+                    alert("El valor de compra de "+cantidad + " unidades de " +ingreso + " por un valor unitario de $" +valor +" es: $" +importe+"\n Lo contactaremos por correo para finalizar el pago y coordinar la entrega. Muchas gracias por su compra!");
+                  }else{
+                    alert("El valor ingresado es incorrecto");
+                  }
+                  break;
+
+                default:
+                  alert("Bebida no encontrada!");
+                  salida = 0;
+              }
+              //Fin Switch para eleccion de bebidas
+          }else if(pedido === 2){
+              let consulta = prompt("Ingrese la bebida que desea consultar: "+bebidas.map((bebida) => bebida.nombre)).toLocaleUpperCase();
+              let bebida = bebidas.find(item => item.nombre === consulta);
+              if(bebida != undefined){
+                  mensaje = `
+                    Nombre: ${bebida.nombre}
+                    Precio: $${bebida.precio}
+                    Cantidad: ${bebida.cant}
+                  `;
+              }else{
+                  alert("la opcion ingresada es invalida");
+                  break;
+              }
+              alert(mensaje);
+              recorroArray = parseInt(prompt("Quiere ver todos nuestros productos, cantidades y precios? ingrese 1 por Si, 2 por No"));
+              if(recorroArray === 1){
+                  bebidas.forEach(bebida => {
+                    concat = concat + ("\n Nombre: "+bebida.nombre + ", Cantidad: "+bebida.cant+", Precio: $"+bebida.precio);
+                  });
+                  alert(concat)
+                  compra = parseInt((prompt("\n Si desea alguno de nuestros productos, ingrese 1, para salir ingrese 2 o precione enter")));
+                  contador = contador + 1;
+                  if(compra === 1){
+                    flag = 1;
+                    break;
+                  }else{
+                    alert("Muchas gracias por contactarnos!");
+                    break;
+                  }
+              }else if(recorroArray === 2){
+                  alert("Muchas gracias por contactarnos!");
+                  contador = 5;
+                  break;
+              }else{
+                  alert("La opcion ingresada no es valida");
+              }
+          }else{
+                alert("La opcion ingresada no es valida");
+          }
+      }else{
+          alert("El dato ingresado no es correcto");
+          flag = 0;
+      }
+      if(salida == 1){
+        contador = 5;
+        break;
+      }
+    } 
 }
 if (contador!=5){
-  alert("Demasiados intentos incorrectos, vuelva a intentar mas tarde");
+    alert("Demasiados intentos incorrectos, vuelva a intentar mas tarde");
 }
